272713885d139f779c103e0c753e293d0efcb87c
Vita Solomina <vitaliaa@gmail.com>
2021-05-03 09:27:09 -0700

    Moved ManagedCache extension to ManagedCache.swift

--- FeedStoreChallenge/ManagedObjects/ManagedCache.swift
+++ FeedStoreChallenge/ManagedObjects/ManagedCache.swift
@@ -21,3 +21,27 @@ extension ManagedCache {
 	@NSManaged public var timestamp: Date
 	@NSManaged public var images: NSOrderedSet
 }
+
+extension ManagedCache {
+	@discardableResult
+	static func create(with feed: [LocalFeedImage], timestamp: Date, in context: NSManagedObjectContext) -> ManagedCache {
+		let cache = ManagedCache(context: context)
+		let images = feed.map { ManagedFeedImage.create(with: $0, in: context) }
+		cache.images = NSOrderedSet(array: images)
+		cache.timestamp = timestamp
+		return cache
+	}
+
+	static func find(in context: NSManagedObjectContext) throws -> [ManagedCache]? {
+		let cacheFetch: NSFetchRequest = ManagedCache.fetchRequest()
+		return try context.fetch(cacheFetch)
+	}
+
+	static func delete(in context: NSManagedObjectContext) throws {
+		try ManagedCache.find(in: context)?.forEach { context.delete($0) }
+	}
+
+	func feed() -> [LocalFeedImage] {
+		images.compactMap { ($0 as? ManagedFeedImage)?.toLocalFeedImage() }
+	}
+}
